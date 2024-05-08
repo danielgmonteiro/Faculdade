1e1b9f8a801d4eb43264d4633c6f4a5d729bd7ee
Daniel G. Monteiro <daniel.g.monteiro@hotmail.com>
2024-05-08 20:52:02 +0100

    Delete calculadora.py

--- Missao-Pratica-Nivel1-Mundo1/calculadora.py
+++
@@ -1,6 +0,0 @@
-a = eval(input("Informe um valor para A: "))
-b = eval(input("Informe um valor para B: "))
-print(f"Resultado da adição:  {a + b}")
-print(f"Resultado da subtração: {a - b}")
-print(f"Resultado da multiplicação: {a * b}")
-print(f"Resultado da divisão: {a / b}")
