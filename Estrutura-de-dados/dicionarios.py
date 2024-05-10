3c91e27bb74b50c0427e81d4b61869e1eb44ac22
Daniel G. Monteiro <daniel.g.monteiro@hotmail.com>
2024-05-10 13:14:10 +0100

    nova atividade

--- Estrutura-de-dados/dicionarios.py
+++ Estrutura-de-dados/dicionarios.py
@@ -1,8 +1,27 @@
 meu_dicionario = {
-    1:"Python",
-    2 : "Java",
-    3 : "PHP"
+    1 : {"linguagem": "Python"},
+    2 : {"linguagem": "Java"},
+    3 : {"linguagem": "PHP"}
 }
 print(meu_dicionario)
+valor_linguagem = dict()
+for e in meu_dicionario:
+    #valor_linguagem.update(meu_dicionario[e].get("linguagem"))
+    valor = meu_dicionario[e].get("linguagem")
+    valor_linguagem[e] = "linguagem"
+print(valor_linguagem)
 print(type(meu_dicionario))
-print(meu_dicionario.get(value()))
+
+dicionario_rutas = dict()
+dicionario_frutas = {
+    1: {"nome": "limão", "tipo": "ácida"},
+    2: {"nome": "laranja", "tipo": "ácida"},
+    3: {"nome": "manga", "tipo": "semiácida"},
+    4: {"nome": "maça", "tipo": "semiácida"},
+    5: {"nome": "banana", "tipo": "doce"},
+    6: {"nome": "mamão", "tipo": "doce"}
+}
+
+
+
+
