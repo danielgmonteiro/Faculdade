610258d65265dfb3533452a8de772ea988fde5e6
Daniel G. Monteiro <daniel.g.monteiro@hotmail.com>
2024-05-12 10:24:37 +0100

    conclusao da micro atividade4
    Correção e conclusão da micro-atividade 4 . dicionários

--- Estrutura-de-dados/dicionarios.py
+++ Estrutura-de-dados/dicionarios.py
@@ -1,18 +1,15 @@
 meu_dicionario = {
-    1 : {"linguagem": "Python"},
-    2 : {"linguagem": "Java"},
-    3 : {"linguagem": "PHP"}
+    1 : "Python",
+    2 : "Java",
+    3 : "PHP"
 }
 print(meu_dicionario)
-valor_linguagem = dict()
-for e in meu_dicionario:
-    #valor_linguagem.update(meu_dicionario[e].get("linguagem"))
-    valor = meu_dicionario[e].get("linguagem")
-    valor_linguagem[e] = "linguagem"
-print(valor_linguagem)
 print(type(meu_dicionario))
+valor_linguagem = meu_dicionario.get(1)
+print(f"Valor da chave 'Linguagem': {valor_linguagem}")
 
-dicionario_rutas = dict()
+dicionario_frutas = dict()
+print("\n")
 dicionario_frutas = {
     1: {"nome": "limão", "tipo": "ácida"},
     2: {"nome": "laranja", "tipo": "ácida"},
@@ -21,7 +18,6 @@ dicionario_frutas = {
     5: {"nome": "banana", "tipo": "doce"},
     6: {"nome": "mamão", "tipo": "doce"}
 }
+print(f"Chave 1:\nNome: {dicionario_frutas[1]['nome']}\nTipo: {dicionario_frutas[1]['tipo']}\n")
 
-
-
-
+print(f"Chave 2:\nNome: {dicionario_frutas[2]['nome']}\nTipo: {dicionario_frutas[2]['tipo']}\n")
