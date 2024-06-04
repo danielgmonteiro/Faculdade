29872e0cbf31aa47e056cd47d0681f47d2b93f6e
Daniel G. Monteiro <daniel.g.monteiro@hotmail.com>
2024-06-04 18:28:06 +0100

    ..

--- Desvendando_essa_caixa_preta/calculadora.py
+++ Desvendando_essa_caixa_preta/calculadora.py
@@ -1,11 +1,11 @@
-    # Solicita ao usuário que insira um valor para 'A' e o converte para um número.
-    a = eval(input("Informe um valor para A: "))
+# Solicita ao usuário que insira um valor para 'A' e o converte para um número.
+a = eval(input("Informe um valor para A: "))
 
-    # Solicita ao usuário que insira um valor para 'B' e o converte para um número.
-    b = eval(input("Informe um valor para B: "))
+# Solicita ao usuário que insira um valor para 'B' e o converte para um número.
+b = eval(input("Informe um valor para B: "))
 
-    # Realiza as operações matemáticas e imprime os resultados.
-    print(f"Resultado da adição:  {a + b}")
-    print(f"Resultado da subtração: {a - b}")
-    print(f"Resultado da multiplicação: {a * b}")
-    print(f"Resultado da divisão: {a / b}")
+# Realiza as operações matemáticas e imprime os resultados.
+print(f"Resultado da adição:  {a + b}")
+print(f"Resultado da subtração: {a - b}")
+print(f"Resultado da multiplicação: {a * b}")
+print(f"Resultado da divisão: {a / b}")
